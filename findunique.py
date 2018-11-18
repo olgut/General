f045c94d841b0d768573c2310fb69b4b62acd76e
Allgoo <31838919+olgut@users.noreply.github.com>
2018-11-18 15:33:11 -0800

    Create findunique.py

---
+++ findunique.py
@@ -0,0 +1,7 @@
+def findunique(array):
+    if len(array)==1:
+        return array
+    for i in range(len(array)):
+        duplicate =[j for j in array if j==array[i]]
+        if len(duplicate)<2:
+            return duplicate
