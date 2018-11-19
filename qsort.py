68eb949f44decbf0806375195c4b0f37cc9ee65a
Allgoo <31838919+olgut@users.noreply.github.com>
2018-11-18 21:47:19 -0800

    Create qsort.py

---
+++ qsort.py
@@ -0,0 +1,14 @@
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    print(left)
+    print(middle)
+    print(right)
+    print("loop number = ")
+    return quicksort(left) + middle + quicksort(right)
+
+print(quicksort([3,6,8,10,11,1,2,1]))
